41cb87e6cf0c9662ceed04782e1bae9313089ce4
moibrahim-applied <mohamed.ibrahim@aaico.com>
2026-03-24 11:58:14 +0400

    chore: bump version to 0.1.1

--- src/opus_aaico/__init__.py
+++ src/opus_aaico/__init__.py
@@ -1,6 +1,6 @@
 """Unofficial Python SDK for the OPUS workflow automation platform."""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from opus_aaico._async import AsyncOpusClient
 from opus_aaico._exceptions import (
